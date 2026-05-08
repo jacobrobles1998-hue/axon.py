6d648f5d1279a5957844d1357570c29e1a73653e
jacobrobles1998-hue <jacobrobles1998@gmail.com>
2026-05-08 14:39:02 -0500

    Update modulodashboard.py
    Added button configuration for dashboard navigation and user settings.

--- modulos/modulodashboard.py
+++ modulos/modulodashboard.py
@@ -3,7 +3,31 @@ import streamlit as st
 def mostrar_dashboard():
     st.title("🚀 AXON - DASHBOARD")
     st.subheader("Estado Actual: Jacob")
-    
+
+# 1. CONFIGURACION BOTONES APP
+#¿que mostrar según el botón?
+
+if selected == "dashboard"
+   mostrar _dashboard() # llama a tu otro archivo 
+
+elif selected == "nutricion"
+   #aqui puedes llamar a 
+mostrar_nutricion() o poner codigo directo
+st.title("registro de nutrición")
+
+elif selected == "configuración"
+# TODO ESTO VA DIRECTO A LA APP.PY
+st.title("configuración") 
+    st.subheader("ajustes del ciclo Diario")
+hora_despertar = st.time_input("¿a que hora te levantaste de la cama hoy?")
+
+if hora de despertar :
+     st.success(f"configuracion guardada para las {hora_desptertar.strftime('%H:
+     %M')}")
+
+         st.divider()
+         st.write("aqui podrás ajustar tus metas de volumen mas adelante.")
+                                                                                             
     # Ejemplo de métricas rápidas
     col1, col2, col3 = st.columns(3)
     with col1:
