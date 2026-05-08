66c671c6b0d92710a3614ceff5ab597a5cb835e9
jacobrobles1998-hue <jacobrobles1998@gmail.com>
2026-05-08 14:47:44 -0500

    Update app.py
    Added configuration options for buttons and user input.

--- app.py
+++ app.py
@@ -19,8 +19,34 @@ selected = option_menu(
     options=["Dashboard", "Nutrición", "configuración"],
     icons=["house", "egg-fried", "activity"],
     orientation="horizontal"
-)
+    )
 
 # 5. ¿Qué mostrar según el botón?
 if selected == "Dashboard":
     mostrar_dashboard()
+    
+# 1. CONFIGURACION BOTONES APP
+#¿que mostrar según el botón?
+
+if selected == "dashboard"
+   mostrar _dashboard() # llama a tu otro archivo 
+
+elif selected == "nutricion"
+   #aqui puedes llamar a 
+mostrar_nutricion() o poner codigo directo
+st.title(" 🍎 registro de nutrición")
+
+elif selected == "configuración"
+# TODO ESTO VA DIRECTO A LA APP.PY
+st.title("configuración") 
+    st.subheader("ajustes del ciclo Diario")
+hora_despertar = st.time_input("¿a que hora te levantaste de la cama hoy?")
+
+if hora de despertar:
+    st.success(f"configuracion guardada para las {hora_desptertar.strftime('%H:
+    %M')}")
+
+        st.divider()
+        st.write("aqui podrás ajustar tus metas de volumen mas adelante.")
+
+
